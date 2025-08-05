21f9bd677e60e6a66f14217bb9cc198424f02fc4
ExileQ2 <minhquan.96vn@gmail.com>
2025-08-05 09:44:36 +0700

    bản 0.0.0.0 listening on all devices

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using System.Net;
+using System.Net.Sockets;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Listen on all network interfaces (localhost and LAN IPs)
+builder.WebHost.UseUrls("http://0.0.0.0:5080");
 
 // Add services to the container.
 
@@ -9,17 +15,31 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+// Always enable Swagger (for internal/testing use only)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Automatically open browser on startup only in Development environment
+if (app.Environment.IsDevelopment())
+{
+    try
+    {
+        // Get local IPv4 address
+        string localIp = Dns.GetHostEntry(Dns.GetHostName())
+            .AddressList
+            .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork)?.ToString() ?? "localhost";
+        var url = $"http://{localIp}:5080/swagger";
+        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        });
+    }
+    catch { /* Ignore errors */ }
+}
 
 app.Run();
